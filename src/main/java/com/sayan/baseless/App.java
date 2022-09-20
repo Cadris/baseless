fafe011d4ba1a4c479b4db19541763b896705c4f
Cadris <cadrissilver@gmail.com>
2022-09-20 18:47:57 +0530

    Innitial Commit

---
+++ src/main/java/com/sayan/baseless/App.java
@@ -0,0 +1,13 @@
+package com.sayan.baseless;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
